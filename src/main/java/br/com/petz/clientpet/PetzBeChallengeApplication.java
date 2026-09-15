fa1f20088401156a9665836e2f1e95dee00170fa
bernardommecabo <bernardomecabo09@gmail.com>
2026-09-15 15:18:27 -0300

    first commit & pg docker container

---
+++ src/main/java/br/com/petz/clientpet/PetzBeChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.petz.clientpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetzBeChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetzBeChallengeApplication.class, args);
+    }
+
+}
