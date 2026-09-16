fe49883acc0242d1fc39f2b313b069b0db9fa7be
bernardommecabo <bernardomecabo09@gmail.com>
2026-09-15 22:15:14 -0300

    feat(api): client api contract

---
+++ src/main/java/br/com/petz/clientpet/client/application/DTOs/ClientResponse.java
@@ -0,0 +1,4 @@
+package br.com.petz.clientpet.client.application.DTOs;
+
+public record ClientResponse() {
+}
---
+++ src/main/java/br/com/petz/clientpet/client/application/api/ClientAPI.java
@@ -0,0 +1,19 @@
+package br.com.petz.clientpet.client.application.api;
+
+import br.com.petz.clientpet.client.application.DTOs.ClientRequest;
+import br.com.petz.clientpet.client.application.DTOs.ClientResponse;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Validated
+@RequestMapping("/v1/client")
+public interface ClientAPI {
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    ClientResponse createClient(@RequestBody @Valid ClientRequest request);
+
+
+}
