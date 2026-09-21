022cc819fabea6a615ef55a5b19dbbad73283da4
bernardommecabo <bernardomecabo09@gmail.com>
2026-09-21 16:09:10 -0300

    fix(pet-api): created PetAPI and removed validated annotation from ClientAPI

---
+++ src/main/java/br/com/petz/clientpet/pet/application/api/PetAPI.java
@@ -0,0 +1,9 @@
+package br.com.petz.clientpet.pet.application.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public interface PetAPI {
+}
